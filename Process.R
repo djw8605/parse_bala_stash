926df24c8991bc5e5c6fcfe8ce217dfb2b83eeca
Derek Weitzel <djw8605@gmail.com>
2016-03-10 17:05:55 -0600

    Adding R processing

---
+++ Process.R
@@ -0,0 +1,22 @@
+
+data = read.csv("~/tmp/stuff.csv")
+
+library(ggplot2)
+library(scales)
+
+data$difference <- data$difference * 15
+
+data$percent <- data$difference / sum(data$difference)
+
+m <- ggplot(data, aes(x=difference)) +
+  geom_bar(aes(y = (..count..)/sum(..count..), fill= (..count..)/sum(..count..)), binwidth=15) +
+  scale_fill_gradient("Frequency", low = "green", high = "red") +
+  ylab("Frequency") +
+  xlab("Delay in minutes") +
+  scale_y_continuous(labels=percent)
+  
+m
+  #scale_y_continuous(labels=percent)
+#m + geom_histogram(aes(fill = ..count..), binwidth = 15) + 
+#  scale_fill_gradient("Count", low = "green", high = "red")
+
